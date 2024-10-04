40c28c339d3af8e010997a37eb44ce63082fa631
Abovetous <nastena.kozarezova@gmail.com>
2024-10-04 17:20:27 +0300

    Add files via upload

---
+++ modul_1_4.py
@@ -0,0 +1,7 @@
+#my_string=input('привет, как тебя зовут? ')
+#print(len(my_string))
+#my_string=input('привет, как тебя зовут?')
+#my_string=input("привет как тебя зовут".upper().lower().replace(' ','')[0:6],)
+my_sstring=input('привет, как тебя зовут?'[-1])
+
+
